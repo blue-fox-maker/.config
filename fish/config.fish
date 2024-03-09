f378eb415dd96a1afe65f46b9f70b9231d544b76
lne <bluefoxtumbleweed@outlook.com>
2024-03-09 15:08:18 +0800

    null

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    cd ~
+    starship init fish | source
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2024-02-02 12:17:33
+set PATH $PATH /home/lne/.local/bin
---
+++ nvim.bak/lua/config/options.lua
@@ -0,0 +1,10 @@
+local o = vim.opt
+
+-- line number
+o.number = true
+o.numberwidth = 2
+o.ruler = false
+
+o.termguicolors = true
+o.showmode = false
+
